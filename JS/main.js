a6c364a77aaa598da9b94d42e40805c85c75e7f6
JurayevOgabek04 <jurayevogabek0304>
2022-04-01 10:41:57 +0500

    Js kod

---
+++ JS/main.js
@@ -0,0 +1,71 @@
+a = 5;
+console.log(a)
+console.log(typeof(a))
+
+// type number(integer)
+
+b = 5.5;
+console.log(b)
+console.log(typeof(b))
+
+// type number(float)
+
+c = "Hello world"
+console.log(c)
+console.log(typeof(c))
+
+// type string
+
+d = true;
+D = false;
+console.log(d , D)
+console.log(typeof(d, D))
+
+// type boolean true and float
+
+e = 1n;
+console.log(e)
+console.log(typeof(e))
+
+// type bigint
+
+
+let f;
+console.log(f)
+
+// type undefined
+
+let g = null;
+console.log(g)
+console.log(typeof(g))
+
+// type null
+
+let w = Symbol("Moy perviy uchitel Anvar Narzullaev");
+console.log(w)
+console.log(typeof(w))
+
+// 
+
+let info ={
+    "ism" : "og'abek",
+    "familiya" : "Hotamov",
+    "ochestva" : "Bobonazar og'li",
+    "t_yil_oy_kun" : 2000_03_04,
+    "age" : 22,
+    "univer" : "TUIT , TATU", 
+    "o'quv markaz" : "Na'jot talim Xadra filiyali",
+    "yo'alish" : "Fullstack developer"
+}
+
+console.log(info)
+console.log(typeof(info))
+
+
+
+// test
+
+i = 5;
+i += 1;
+console.log(i)
+
